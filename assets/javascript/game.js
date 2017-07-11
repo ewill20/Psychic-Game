b71449af89b97de32f05e9ac16888949693bd230
ewill20 <ewill2020@gmail.com>
2017-07-11 14:22:10 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,88 @@
-document.onkeypress = function(event) {
+
+$( document ).ready(function() {
+
+	// Sets all the default values for wins, losses, guesses remaining, and an empty guesses variable //
+	var wins = 0;
+	var losses = 0;
+	var guessesRem = 8;
+	var guesses = " ";
+
+	// Array containing all the possible choices for the gypsy //
+	var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+
+    // Chooses a random number from the alphabet as the number to be guessed //
+	var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+	// Logs the random number to the console //
+    console.log(computerChoice)
+
+
+   
+
+    // Resets the game for a loser //
+function resetL() {
+	computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+	console.log(computerChoice);
+	guessesRem = 8;
+	$("span#guesses").text("Your Guesses So Far: ");
+	$("span#guessesL").text("Guesses Left: 9");
+};
+
+	// Resets the game for a winner //
+	function resetW() {
+		computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+		console.log(computerChoice);
+		guessesRem = 9;
+		$("span#guesses").text("Your Guesses So Far: ");
+		$("span#guessesL").text("Guesses Left: 9");
+	};
+   	// Adds wins to the user total.
+    function winner() {
+        wins++;
+        alert("You Win!")
+        $("span#wins").text("Wins: " + wins);
+        resetW();
+    };
+    // Adds losses to the user total.
+    function loser() {
+    	losses++;
+    	alert("You Lose Sucker!")
+    	alert("The Answer Was: " + computerChoice);
+    	$("span#losses").text("Losses: " + losses);
+    	resetL();
+    };
+
+
+
+	// Tracks which key the user pressed //
+	document.onkeypress = function(event) {
     var userGuess = event.key;
 
-    if(userGuess === computerChoice){
-        wins++;
-    }else{
-        guesses--;
+    // Logs the user input to the console //
+    console.log(userGuess);
+
+    // Adds the user's guesses into the area next to "Your Guesses So Far:" //
+    $("span#guesses").append(" " + userGuess);
+
+    // Visually shows the amount of guesses remaining each time the user clicks a key //
+	$("span#guessesL").text("Guesses Left: " + guessesRem);
+
+    
+   	// Loop to set parameters for the game //
+    for(var i = 0; i < 1; i++) {
+
+    if(userGuess === computerChoice) {
+    	winner();
     }
-
-    if(guesses === 0){
-        losses++
+    if(guessesRem === 0) {
+        loser();
     }
-
-    document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "losses: " + losses;
-    document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
-
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-// 	<script type="text/javascript">
-	
-
-// 	// Variables
-// 	//=============
-// 	var wins = 0;
-
-// 	var losses = 0;
-
-// 	var guesses = 0;
-
-// 	var guessesRem = 9;
-
-// 	var randomGuess = Math.round(Math.random() * 26);
-
-// 	$(document).ready(function() {
-
-// 	// This jQuery function reduces the guesses remaining by 1 and increases the total guesses by one when any of the items from the arr is entered.
-// 	$("guessesAll").on("keypress", function() {
-// 		guesses++;
-// 		guessesRem--;
-// 	})
-// });
-
-// 	// This is to establish the array of potential guesses.
-// 	var guessesAll = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
-
-// 	for (var i = 0; i < guessesAll.length; i++);
-
-// 	document.onkeyup = function(event) {
-
-// 		var userInput = event.key;
-
-// 		// This increases the wins if the user guesses correctly
-// 		if (userInput === "randomGuess") {
-// 			wins++;
-// 			alert("You guessed correctly!")
-// 		};
-		
-// 		// This increases the amount of guesses if the user guesses incorrectly and decreases the guesses remaining.
-// 		if (userInput != "randomGuess") {
-// 			guesses++;
-// 			guessesRem--;
-// 		};
-
-// 		// This function will increase losses by 1 if the user guesses incorrectly 9 times.
-// 		if ((userInput != "randomGuess") * 9 ){
-// 			losses++;
-// 			alert("Sorry, You Lose!")
-// 		};
-
-	
-	
-
-	// function makeid() {
-  	// var text = "";
-  	// var possible = "abcdefghijklmnopqrstuvwxyz";
-
-  	
-
-  	// return text;
-	}
-
-	// console.log(makeid());
-
-</script>
+    if(userGuess != computerChoice) {
+        guessesRem--;
+    }
+  }
+};
+})
